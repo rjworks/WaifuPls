c4d4f416920e9c40181bb6a609987f8bd7b6ba1d
rjworks <arjayandal05@yahoo.com>
2021-02-27 21:14:01 -0600

    First working bot

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-const Discord = require('discord.js');
-const axios = require('axios')
-const client = new Discord.Client();
-const fs = require('fs');
+const discord = require( 'discord.js' );
+const client = new discord.Client();
+const fs = require( 'fs' );
+const WaifuApi = require( "./WaifuApi" );
+const config = JSON.parse( fs.readFileSync( "config.json", "utf-8" ) );
+const waifuApi = new WaifuApi( config.mywaifulist_key )
 
-let token = "";
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+client.on( 'ready', () => {
+    console.log( `Logged in as ${ client.user.tag }!` );
+} );
 
-client.on('message', msg => {
-    if (msg.content === 'waifupls') {
-        //console.log(getRandomWaifu())
-        msg.reply('pong');
+client.on( 'message', msg => {
+    if ( msg.content === 'waifupls' || msg.content === 'waifpls' ) {
+        waifuApi.getRandomWaifu().then( ( waifu ) => {
+            // normally this happens when there's too much requests
+            if ( waifu == null ) {
+                msg.reply( "Oop! Pls slow down :)" )
+                return;
+            }
+            // just so that it doesn't show nothing when an anime/waifu doesn't have a description
+            const animeDescription = waifu.appearances[ 0 ].description == "" ? "No description given :(" : waifu.appearances[ 0 ].description;
+            const waifuDescription = waifu.description == "" ? "No description given :(" : waifu.description;
+            const pfp = 'https://github.com/rjworks/WaifuPls/blob/master/pfp.jpg?raw=true';
+            // embedded reply
+            const exampleEmbed = new discord.MessageEmbed()
+                .setColor( '#f59542' )
+                .setTitle( waifu.name )
+                .setURL( waifu.url )
+                .setAuthor( 'WaifuPls', pfp, 'https://github.com/rjworks/WaifuPls' )
+                .setDescription( waifuDescription )
+                .addFields(
+                    {
+                        name: `Anime`,
+                        value: `[${ waifu.appearances[ 0 ].name }](${ waifu.appearances[ 0 ].url })` + " - " + animeDescription
+                    },
+                    { name: 'Likes', value: waifu.likes },
+                    { name: 'Trash', value: waifu.trash },
+                )
+                .setImage( waifu.display_picture )
+                .setTimestamp()
+                .setFooter( 'WaifuPls', pfp );
+            msg.reply( exampleEmbed )
+        } )
     }
 });
 
-fs.readFileSync('token.txt', 'utf-8').split(/\r?\n/)
-    .forEach(function(line){
-        token = line;
-});
 
-
-client.login( token );
+client.login( config.bot_token );
